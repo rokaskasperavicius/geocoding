0382a75857ab8d1306d863dd8623615022d99591
Rokas Kasperavicius <goodname258@gmail.com>
2021-10-22 14:52:33 +0200

    stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+export const App = () => {
+  const [location, setLocation] = useState()
+
+  const [data, setData] = useState()
+
+  const [weather, setWeather] = useState()
+
+  const sendLocation = () => {
+    fetch(`https://nominatim.openstreetmap.org/search?q=${location}&format=json`)
+      .then(res => res.json())
+      .then(dataJson => setData(dataJson))
+  }
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=06d73ad44324084601b8cbbe52ab44b2`)
+        .then(res => res.json())
+        .then(dataJson => setWeather(dataJson))
+    }
+  }, [data])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={location} onChange={(e) => setLocation(e.target.value)} />
+
+      <button onClick={() => sendLocation()}>Get info</button>
+
+      {data && data.length > 0 && (
+        <div>{data[0].lat} {data[0].lon}</div>
+      )}
+      <br/>
+      {weather && (
+        <div>{weather.main.temp - 273.15}</div>
+      )}
     </div>
   );
 }
-
-export default App;
