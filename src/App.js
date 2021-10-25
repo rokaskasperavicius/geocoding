054c0c8ba44f0a28e962df06c7b812cc7076970a
Rokas Kasperavicius <goodname258@gmail.com>
2021-10-25 21:51:15 +0200

    a lot of shit

--- src/App.js
+++ src/App.js
@@ -3,41 +3,72 @@ import { Map, Marker, ZoomControl, Overlay } from 'pigeon-maps'
 import { osm } from 'pigeon-maps/providers'
 import { useDebounce } from 'use-debounce'
 
+import CircleLoader from "react-spinners/CircleLoader";
+
+import Icon from './target.svg'
+import ExpandIcon from './expand-icon.svg'
+
 export const App = () => {
   const [location, setLocation] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [isOpen, setIsOpen] = useState(true)
 
   const [data, setData] = useState()
   const [weather, setWeather] = useState()
 
   const [center, setCenter] = useState([40.758091, -74.000267])
   const [anchor, setAnchor] = useState(center)
-  const [zoom, setZoom] = useState(15)
+  const [zoom, setZoom] = useState(17)
 
-  const [displayNames, setDisplayNames] = useState([])
+  const [displayNames, setDisplayNames] = useState()
 
   const [debouncedValue] = useDebounce(location, 1000);
 
-  const sendLocation = (location) => {
-    if (location.length > 0) {
-      fetch(`https://nominatim.openstreetmap.org/search?q=${location}&format=json`)
+  // const sendLocation = (location) => {
+  //   if (location.length > 0) {
+  //     fetch(`https://nominatim.openstreetmap.org/search?q=${location}&format=jsonv2`)
+  //       .then(res => res.json())
+  //       .then(dataJson => {
+  //         console.log(dataJson)
+  //         setZoom(17)
+  //         setData(dataJson[0])
+  //         setCenter([parseFloat(dataJson[0].lat), parseFloat(dataJson[0].lon)])
+  //         setAnchor([parseFloat(dataJson[0].lat), parseFloat(dataJson[0].lon)])
+  //       })
+  //       .finally(() => {
+  //         console.log("SETTING TO FALSE")
+  //         setIsLoading(false)
+  //       })
+  //   }
+  // }
+
+  const reverseLookUp = () => {
+    setIsLoading(true)
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`)
         .then(res => res.json())
         .then(dataJson => {
           console.log(dataJson)
-          setZoom(15)
+          setZoom(17)
           setData(dataJson)
-          setCenter([parseFloat(dataJson[0].lat), parseFloat(dataJson[0].lon)])
-          setAnchor([parseFloat(dataJson[0].lat), parseFloat(dataJson[0].lon)])
+          setCenter([parseFloat(dataJson.lat), parseFloat(dataJson.lon)])
+          setAnchor([parseFloat(dataJson.lat), parseFloat(dataJson.lon)])
         })
-    }
+        .finally(() => {
+          console.log("SETTING TO FALSE")
+          setIsLoading(false)
+        })
+    });
   }
 
   useEffect(() => {
-    if (data && data.length > 0) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=06d73ad44324084601b8cbbe52ab44b2`)
+    if (center) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${center[0]}&lon=${center[1]}&appid=06d73ad44324084601b8cbbe52ab44b2`)
         .then(res => res.json())
         .then(dataJson => setWeather(dataJson))
     }
-  }, [data])
+  }, [center])
 
   function mapTiler (x, y, z, dpr) {
     return `https://maptiles.p.rapidapi.com/en/map/v1/${z}/${x}/${y}.png?rapidapi-key=5e03c7f7cemsh40721afc8a3ddc5p1e7af4jsnd63ea410a5c0`
@@ -45,55 +76,98 @@ export const App = () => {
 
   useEffect(() => {
     if (debouncedValue.length > 0) {
-      fetch(`https://nominatim.openstreetmap.org/search?q=${debouncedValue}&format=json`)
+      fetch(`https://nominatim.openstreetmap.org/search?q=${debouncedValue}&format=jsonv2`)
         .then(res => res.json())
         .then(dataJson => {
           setDisplayNames(dataJson)
         })
+        .catch(() => console.log("ERRORRORORO"))
+        .finally(() => {
+          console.log("SETTING TO FALSE")
+          setIsLoading(false)
+        })
     }
   }, [debouncedValue])
 
-  return (
-    <div>
-      <div>
-        <input value={location} onChange={(e) => setLocation(e.target.value)} />
-        {/* <button onClick={() => sendLocation()}>Get info</button> */}
+  useEffect(() => {
+    if (location.length > 0) {
+      setIsLoading(true)
+    } else {
+      setDisplayNames(undefined)
+      setIsLoading(false)
+    }
+  }, [location])
 
-        {data && data.length > 0 && (
-          <div>{data[0].lat} {data[0].lon}</div>
-        )}
-        <br/>
-        {weather && (
-          <div>{weather.main.temp - 273.15}</div>
-        )}
+  console.log({ data, isLoading })
+
+  return (
+    <div className="app">
+      <div className={`search-wrapper${isOpen ? ' search-wrapper-open' : ''}`}>
+        <div className="search-content">
+          <div className="input-wrapper">
+            <input value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Search for an address" />
+            <img
+              onClick={reverseLookUp}
+              alt="location icon"
+              className="input-icon"
+              src={Icon}
+            />
+          </div>
+
+          {isLoading && (
+            <CircleLoader
+              size={80}
+              css={{
+                display: "block",
+                margin: "auto",
+                marginTop: "100px",
+              }}
+            />
+          )}
+
+          {displayNames && !isLoading && (
+            <ol className="addresses">
+              {displayNames.map(({ display_name, lat, lon }) => (
+                <li className="address" onClick={() => setCenter([parseFloat(lat), parseFloat(lon)])}>{display_name}</li>
+              ))}
+            </ol>
+          )}
+
+          {displayNames && displayNames.length === 0 && !isLoading && (
+            <div className="no-address">No address found</div>
+          )}
+        </div>
       </div>
-      {displayNames.map(({ display_name }) => (
-        <div style={{ cursor: "pointer" }} onClick={() => sendLocation(display_name)}>{display_name}</div>
-      ))}
-      <div className="map">
+      <div className="map-wrapper">
         <Map
-          height={800}
+          height="100%"
           provider={osm}
           center={center}
           zoom={zoom}
-          // zoomSnap={false}
-          onBoundsChanged={({ center, zoom }) => {
-            // setZoom(zoom)
-            // setCenter(center)
-          }}
+          metaWheelZoom={true}
         >
-           {/* <Overlay anchor={anchor} latLngToPixel={(...props) => console.log(props)}>
-            {/* <img src='https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg' width={240} height={158} alt='' /> */}
-            {/* <div>
-              <svg width={zoom === 18 ? 100 : 50} height={zoom === 18 ? 100 : 50}>
-                <circle cx="20" cy="20" r={zoom * 16} stroke="green" stroke-width="4" fill="yellow" />
-              </svg>
-            </div> */}
-          {/* </Overlay> */}
-          <ZoomControl />
-          <Marker width={50} anchor={anchor}/>
+          <img
+            src={ExpandIcon}
+            alt="expand icon"
+            className="expand-icon"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          {center && (
+            <div className="position">
+              Latitude: {center[0]}&nbsp;&nbsp;|&nbsp;&nbsp;Longitude: {center[1]}
+            </div>
+          )}
+          {weather && (
+            <div className="temperature">
+              {Math.round(weather.main.temp - 273.15)} °C
+            </div>
+          )}
+          <ZoomControl style={{ bottom: 30, top: "unset", right: 10, left: "unset" }} buttonStyle={{ width: 50, height: 50, minWidth: "unset" }} />
+          <Marker width={50} anchor={center}/>
         </Map>
       </div>
     </div>
   );
 }
+
+
--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import './main.css'
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <footer>
+      <div>
+        © 2021. All Rights Reserved
+      </div>
+      <div>
+        Created by <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rokas-kasperavi%C4%8Dius-a70458158">Rokas Kasperavicius</a> and <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rasgjen/">Rasmus G. Jensen</a>
+      </div>
+    </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
