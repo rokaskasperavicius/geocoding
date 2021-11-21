7d3a4558d49c43de1f17b90f0ccb0d4136e2a6ac
Rokas Kasperavicius <goodname258@gmail.com>
2021-11-21 17:00:32 +0100

    Please

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -271,7 +271,7 @@ export const Map = ({ setInverted }) => {
   return (
     <main className='map' onMouseMove={(event) => setPos([event.clientX, event.clientY])}>
       <img crossOrigin='anonymous' id="test" className='image-tile' src={image} />
-      <button onClick={() => console.log('Predicting')}>CLICK</button>
+      <button onClick={() => console.log('Predicting 123')}>CLICK</button>
       {/* <div
         style={{
           width: '200px',
